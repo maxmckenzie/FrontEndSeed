27c1c9c876f9096f49c6ed33353f51e075af79d1
Max McKenzie <you@example.com>
2016-08-29 15:03:38 +0100

    corrected referance to localhost

--- app/index.js
+++ app/index.js
@@ -16,8 +16,8 @@ app.get('/', function(req, res){
   });
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log(`Node app is running on http://localhost:${app.get('port')}`);
 });
